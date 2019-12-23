118d3405ed3bbfde62e319d17f56275040f234e0
tahaShaheen <taha.shaheen@live.com>
2019-12-23 17:41:52 +0500

    trying to make sense of threads and handlers

--- basicBluetoothChat_server/app/src/main/java/com/example/basicbluetoothchat_server/MainActivity.java
+++ basicBluetoothChat_server/app/src/main/java/com/example/basicbluetoothchat_server/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.basicbluetoothchat_server;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,15 +13,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.widget.TextView;
 
 import java.io.IOException;
 import java.util.UUID;
 
+
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "Main-Activity";
+    private static final String TAG = "MY_APP_DEBUG_TAG";
     private static final int REQUEST_ENABLE_BT = 1;
     private static final int REQUEST_BT_DISCOVERABILITY_ENABLE = 2;
     private static final int TIME_DURATION_FOR_DISCOVERABILITY = 300;
@@ -183,8 +189,7 @@ public class MainActivity extends AppCompatActivity {
                     Log.d(TAG, "A connection was accepted!");
                     // A connection was accepted. Perform work associated with
                     // the connection in a separate thread.
-
-//                    manageMyConnectedSocket(socket);
+                    manageMyConnectedSocket(socket);
                     try {
                         mmServerSocket.close();
                         Log.d(TAG, "Socket's close() method successful");
@@ -195,7 +200,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
-
 
         // Closes the connect socket and causes the thread to finish.
         public void cancel() {
@@ -209,5 +213,33 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void manageMyConnectedSocket(BluetoothSocket socket){
+
+        final int MESSAGE_READ = 0;
+        final int MESSAGE_WRITE = 1;
+        final int MESSAGE_TOAST = 2;
+
+        Handler handler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+
+                switch (msg.what){
+                    case MESSAGE_READ:
+                        Log.d(TAG, "MESSAGE_READ");
+                        break;
+                    case MESSAGE_WRITE:
+                        Log.d(TAG, "MESSAGE_WRITE");
+                        break;
+                    case MESSAGE_TOAST:
+                        Log.d(TAG, "MESSAGE_TOAST");
+                        break;
+                }
+                super.handleMessage(msg);
+            }
+        };
+
+        MyBluetoothService myBluetoothService = new MyBluetoothService(socket, handler);
+
+    }
 
 }
